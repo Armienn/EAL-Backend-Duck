a67cb7c887910eaf5614d0d78eb7eb64584ef2cd
Armienn <nigathr@gmail.com>
2015-09-22 21:31:21 +0200

    Added link to register page + beautification

--- index.php
+++ index.php
@@ -7,12 +7,15 @@
 	</head>
 	<body>
 		<div class="login-card">
-			<h1>Log-in</h1>
+			<h1>Log in</h1>
 			<br>
 			<form action="login.php" method="post">
 				<input type="text" name="user" placeholder="Username">
 				<input type="password" name="pass" placeholder="Password">
-				<input type="submit" name="login" class="login login-submit" value="login">
+				<input type="submit" name="login" class="login login-submit" value="Log in">
+			</form>
+			<form action="registerpage.php">
+				<input type="submit" class="login login-submit" value="Register">
 			</form>
 			<div class="login-help">
 			<?php if(isset($_COOKIE["user"]))
